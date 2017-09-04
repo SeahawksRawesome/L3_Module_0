8bc975e1a3ec442d977424076ce13b67c06e56c5
League <ewbrowne743@gmail.com>
2017-09-03 17:30:02 -0700

    gg
    gg didn’t finish stuff. Work from 6 on

--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,17 +1,23 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
+		ArrayList<String> mehList = new ArrayList<String>();
+		mehList.add("Big");
+		mehList.add("Boi");
+		mehList.add("Brandon");
+		mehList.add("Boughtened");
+		mehList.add("Bhacon");
+	//	for(String s : mehList ) {
+	//		System.out.println(s);
+	//	}
+		for(int i = 4; i > mehList.size(); i--) {
+			String s = mehList.get(i);
+			System.out.println(s);
+		}
 		
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
 		
 		//6. Print all the Strings in reverse order.
 		
